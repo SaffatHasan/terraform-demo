2512117c118e55195a9bf4d9411846c843cd31f6
Saffat Hasan <saffathasan@gmail.com>
2021-08-09 13:56:15 -0400

    feat: add hello world to ec2.example

--- main.tf
+++ main.tf
@@ -6,4 +6,10 @@ resource "aws_instance" "example" {
   # Amazon Machine Image- - Ubuntu 18.04  (free)
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, world!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 }
