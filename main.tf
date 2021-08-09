bb2d2e7d9a0366197bb795752055d172d5dca0bf
Saffat Hasan <saffathasan@gmail.com>
2021-08-09 14:48:44 -0400

    feat: allow ingress to :8080 on ec2.example

--- main.tf
+++ main.tf
@@ -4,12 +4,24 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   # Amazon Machine Image- - Ubuntu 18.04  (free)
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all_ingress_to_8080.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+}
+
+resource "aws_security_group" "allow_all_ingress_to_8080" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
